ffee147ddeaf272bd4e4a0a014055710b24df80c
Dav1nch00 <147347622+Dav1nch00@users.noreply.github.com>
2025-09-19 11:43:23 -0500

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 // routes/auth.js
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/authController");
+const { updateUserController } = require("../controllers/authController");
+const verifyToken = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
@@ -9,5 +11,8 @@ router.post("/register", registerUser);
 
 // Login
 router.post("/login", loginUser);
+
+// Actualizar
+router.put("/update", verifyToken, updateUserController);
 
 module.exports = router;
